6d009f11d8311742a29c9625a963760a255731b3
omiashish2019 <49369670+omiashish2019@users.noreply.github.com>
2020-06-19 11:34:49 +0530

    Swagger

--- GitTesting/Startup.cs
+++ GitTesting/Startup.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using iNube.Utility.Framework.Extensions;
+using iNube.Utility.Framework.LogPrivider.LogService;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 
 namespace GitTesting
 {
@@ -24,17 +29,31 @@ namespace GitTesting
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            //Common Service
+            services.InitializedCommonServices(Configuration);
+
+            services.AddMvc()
+           .AddFluentValidation(options =>
+           {
+               options.RegisterValidatorsFromAssemblyContaining<Startup>();
+           });
+            services.AddHttpsRedirection(options =>
+            {
+                options.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
+                //options.HttpsPort = 5001;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.InitializedCommonConfiguration(env, Configuration);
+            app.ConfigureExceptionHandler(new LoggerManager(Configuration));
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
+           
             app.UseMvc();
         }
     }
