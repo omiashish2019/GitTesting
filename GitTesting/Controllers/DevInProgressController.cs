1238dcc1b37ab6e2142f26825b9ec577cce53ebb
omiashish2019 <49369670+omiashish2019@users.noreply.github.com>
2021-08-27 12:20:30 +0530

    Dev Testing

---
+++ GitTesting/Controllers/DevInProgressController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitTesting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DevInProgressController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "DEv in Progress", "Dev in Progress" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "DEv in Progress";
+        }
+    }
+}
